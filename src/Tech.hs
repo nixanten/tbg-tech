f5c000a3abbf67065d5f069e057c34f73f8264dc
Niklas Xanten <nxanten@gmail.com>
2017-01-10 19:57:35 +0100

    calculating boosted probabilities (two inspiration rolls per turn)

--- src/Tech.hs
+++ src/Tech.hs
@@ -9,15 +9,25 @@ techProbs :: [Int] -> Double -> [Int] -> [(Int, Double)]
 techProbs _ p [] = [(0, p)]
 techProbs (a:advances) prob  techs = 
         let start = step . map (subtract a) $ techs
-            len = length start    
-            p = prob / fromIntegral len
-            tec = [step $ take n start <> ((start !! n) - 5: drop (n+1) start)| n <- [0 .. len-1]]  
+            tec = picks prob start
         in if len == 0 then [(1, prob)] else M.toList . M.fromListWith (+) . map (first (+1)) . concatMap (techProbs advances p) $ tec  
 
+boostProbs :: [Int] -> Double -> [Int] -> [(Int, Double)]
+boostProbs _ p [] = [(0, p)]
+boostProbs (a:advances) prob  techs = 
+        let start = step . map (subtract a) $ techs
+            tec = concatMap (uncurry picks) $ picks prob start
+        in if start == [] then [(1, prob)] else M.toList . M.fromListWith (+) . map (first (+1)) . concatMap (uncurry $ boostProbs advances) $ tec
+        
 step :: [Int] -> [Int]
 step ls = let (finished, remaining) = partition (<=0) ls
           in fillUp remaining $ (-1) * sum finished
 
+picks :: Double -> [Int] -> [(Double, [Int])]          
+picks p techs = let len = length techs
+                    p' = p / fromIntegral len
+                in [(p', step $ take n techs  <> ((techs !! n) - 5: drop (n+1) techs))| n <- [0 .. len-1]]
+          
 fillUp :: [Int] -> Int -> [Int] 
 fillUp [] _ = []
 fillUp ls v | v <= 0 = ls
