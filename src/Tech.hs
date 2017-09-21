8ffb46cb8ae008f13027ba17d8ecc210030a47a0
Niklas Xanten <nxanten@gmail.com>
2017-09-21 15:13:11 +0200

    calculate boosts in arbitrary years

--- src/Tech.hs
+++ src/Tech.hs
@@ -6,28 +6,37 @@ import Data.List
 import qualified Data.Map as M
 
 techProbs :: [Int] -> Double -> [Int] -> [(Int, Double)]
-techProbs _ p [] = [(0, p)]
-techProbs (a:advances) prob  techs = 
-        let start = step . map (subtract a) $ techs
-            tec = picks prob start
-        in if len == 0 then [(1, prob)] else M.toList . M.fromListWith (+) . map (first (+1)) . concatMap (techProbs advances p) $ tec  
+techProbs = techProbsBoosts . (flip zip) (repeat False)
 
 boostProbs :: [Int] -> Double -> [Int] -> [(Int, Double)]
-boostProbs _ p [] = [(0, p)]
-boostProbs (a:advances) prob  techs = 
-        let start = step . map (subtract a) $ techs
-            tec = concatMap (uncurry picks) $ picks prob start
-        in if start == [] then [(1, prob)] else M.toList . M.fromListWith (+) . map (first (+1)) . concatMap (uncurry $ boostProbs advances) $ tec
+boostProbs = techProbsBoosts . (flip zip) (repeat True)
+        
+techProbsBoosts :: [(Int, Bool)] -> Double -> [Int] -> [(Int, Double)]
+techProbsBoosts _ p [] = [(0, p)]
+techProbsBoosts ((a, boost):advances) prob  techs 
+        | start == [] = [(1, prob)]
+        | otherwise = M.toList . M.fromListWith (+) . map (first (+1)) . concatMap (uncurry $ techProbsBoosts advances) $ tec                               
+    where start = step . map (subtract a) $ techs
+          tec   = if boost then concatMap (uncurry picks) $ picks prob start else picks prob start
+
+        
         
 step :: [Int] -> [Int]
 step ls = let (finished, remaining) = partition (<=0) ls
           in fillUp remaining $ (-1) * sum finished
 
-picks :: Double -> [Int] -> [(Double, [Int])]          
+picks :: Double -> [Int] -> [(Double, [Int])]
+picks p [] = [(p, [])]        
 picks p techs = let len = length techs
                     p' = p / fromIntegral len
-                in [(p', step $ take n techs  <> ((techs !! n) - 5: drop (n+1) techs))| n <- [0 .. len-1]]
-          
+                in [(p', step $ subAt n techs 5)| n <- [0 .. len-1]]
+
+  where subAt :: Int -> [Int] -> Int -> [Int]
+        subAt 0 (i:is) s = i - s : is
+        subAt n (i:is) s | n > 0 = i : subAt (n-1) is s
+        subAt _ _ _ = error "subtracting at invalid index"
+
+                
 fillUp :: [Int] -> Int -> [Int] 
 fillUp [] _ = []
 fillUp ls v | v <= 0 = ls
